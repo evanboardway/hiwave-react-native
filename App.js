f58db1d715c08599911df1b63ff79191657d101c
Evan Boardway <evan@Evans-Mac-Pro.local>
2021-10-07 19:11:37 -0400

    installed redux

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const rootReducer = (state = {}, action) => { return state }
+const store = createStore(rootReducer)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>You wanna seee some real speed</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>You wanna seee some real speed</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
@@ -19,3 +26,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+// To connect component to redux.
+// import { connect } from 'react-redux'
+
+// And then calling it with a mapStateToProps or mapDispatchToProps argument, and then passing in the unconnected component:
+
+// const ConnectedLoginGate = connect(state => ({ username: state.auth.username }))(LoginGate)
+
+
+// npx react-native run-ios --simulator

eec242e49deea2d9fbb0e274f2b7b075b3cee1ab
Evan Boardway <evan@Evans-Mac-Pro.local>
2021-10-12 18:47:50 -0400

    added a simple layout to assist future dev

---
+++ src/assets/themes.js
@@ -0,0 +1,9 @@
+// light theme 238, 238, 238
+// Tertiary 30, 40, 54
+// Dark theme 22, 27, 34
+
+export const LIGHT_THEME = 'rgb(238, 238, 238)'
+export const DARK_THEME = 'rgb(22, 27, 34)'
+export const OVERLAY_1 = 'rgba(174, 208, 255, 0.1)'
+export const OVERLAY_2 = 'rgba(174, 208, 255, 0.2)'
+export const BUTTON_ACCENT = 'rgba(143, 11, 158, 0.8)'
--- src/components/controls.js
+++ src/components/controls.js
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, StyleSheet } from 'react-native';
 import { Provider, connect } from 'react-redux';
 import { WSCONNECTED, WSCONNECTING, WSFAILED } from '../helpers/enums';
 import { WebSocketContext } from '../services/websocket';
+import { OVERLAY_1, OVERLAY_2, BUTTON_ACCENT, DARK_THEME } from '../assets/themes';
 
 const buttonText = (wsConnectionState) => {
     switch (wsConnectionState) {
@@ -26,15 +27,30 @@ const ControlsView = (props) => {
     }
 
     return (
-        <View>
-            <Button
-                onPress={sendMessage}
-                title="Connect"
-                color="#841584"
-            />
+        <View styles={styles.controls}>
+            <View style={styles.connectButton}>
+                <Button
+                    onPress={sendMessage}
+                    title="CONNECT"
+                    color='rgba(255, 255, 255, 0.7)'
+                />
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    connectButton: {
+        backgroundColor: BUTTON_ACCENT,
+        margin: 5,
+        paddingHorizontal: 20,
+        paddingVertical: 5,
+        borderRadius: 100,
+        shadowRadius: 20,
+        shadowOpacity: 0.1
+    },
+    controls: {}
+})
 
 const mapStateToProps = (state) => {
     return { wsConnectionState: state.wsConnectionState }

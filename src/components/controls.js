85bce3550d27339f4f96c14c224f9383f5ca521a
Evan Boardway <evan@Evans-Mac-Pro.local>
2021-10-18 14:53:30 -0400

    save state

--- src/components/controls.js
+++ src/components/controls.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { Button, Text, View, StyleSheet } from 'react-native';
 import { connect, useDispatch } from 'react-redux';
-import { WRTC_CONNECTION_REQUESTED, WSCONNECTED, WSCONNECTING, WSFAILED, WRTC_CONNECTING, WRTC_CONNECTED, WRTC_DISCONNECT, WRTC_UPDATE_CONNECTION_STATE, WRTC_CONNECT } from '../helpers/enums';
+import { WRTC_CONNECTION_REQUESTED, WSCONNECTED, WSCONNECTING, WSFAILED, WRTC_CONNECTING, WRTC_ADD_TRACK, WRTC_CONNECTED, WRTC_DISCONNECT, WRTC_UPDATE_CONNECTION_STATE, WRTC_CONNECT } from '../helpers/enums';
 import { OVERLAY_1, OVERLAY_2, BUTTON_ACCENT, DARK_THEME } from '../assets/themes';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
@@ -22,38 +22,72 @@ const ControlsView = (props) => {
             break
     }
     return (
-        <View style={styles.connectButton}>
-            <Button
-                onPress={() => {
-                    if (props.wrtcConnectionState == WRTC_CONNECTED) {
-                        props.dispatch({
-                            type: WRTC_DISCONNECT
-                        })
-                    } else {
-                        props.dispatch({
-                            type: WRTC_CONNECT
-                        })
-                    }
-                }}
-                title={title}
-                color='rgba(255, 255, 255, 0.7)'
-                disabled={disabled}
-            />
+        <View style={styles.controlsContainer}>
+            <View style={styles.secondaryControlsContainer}>
+                <View style={styles.button}>
+                    <Button
+                        title={"VOICE"}
+                        color='rgba(255, 255, 255, 0.7)'
+                        onPress={() => {
+                            props.dispatch({
+                                type: WRTC_ADD_TRACK
+                            })
+                        }}
+                    />
+                </View>
+
+                <View style={styles.button}>
+                    <Button
+                        title={"MUTE"}
+                        color='rgba(255, 255, 255, 0.7)'
+                    />
+                </View>
+
+            </View>
+            <View style={styles.button}>
+                <Button
+                    onPress={() => {
+                        if (props.wrtcConnectionState == WRTC_CONNECTED) {
+                            props.dispatch({
+                                type: WRTC_DISCONNECT
+                            })
+                        } else {
+                            props.dispatch({
+                                type: WRTC_CONNECT
+                            })
+                        }
+                    }}
+                    title={title}
+                    color='rgba(255, 255, 255, 0.7)'
+                    disabled={disabled}
+                />
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    connectButton: {
+    button: {
         backgroundColor: BUTTON_ACCENT,
-        margin: 5,
-        paddingHorizontal: 20,
-        paddingVertical: 20,
-        borderRadius: 100,
+        margin: 12,
+        borderRadius: 30,
         shadowRadius: 20,
-        shadowOpacity: 0.3
+        shadowOpacity: 0.3,
+        flex: 1,
+        flexGrow: 1,
+        justifyContent: 'center'
     },
-    controls: {}
+    controlsContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        width: '100%'
+    },
+    secondaryControlsContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+    }
 })
 
 const mapStateToProps = (state) => {
--- src/store/middleware/wrtcMiddleware.js
+++ src/store/middleware/wrtcMiddleware.js
@@ -1,4 +1,4 @@
-import { UPDATE_WRTC_CONNECTION_STATE, WRTC_ANSWER, WRTC_CONNECT, WRTC_CONNECTED, WRTC_CONNECTING, WRTC_CONNECTION_REQUESTED, WRTC_DISCONNECT, WRTC_DISCONNECTED, WRTC_ICE_CANDIDATE, WRTC_OFFER, WRTC_RENEGOTIATION, WRTC_UPDATE_CONNECTION_STATE, WS_SEND_MESSAGE } from "../../helpers/enums"
+import { UPDATE_WRTC_CONNECTION_STATE, WRTC_ADD_TRACK, WRTC_ANSWER, WRTC_CONNECT, WRTC_CONNECTED, WRTC_CONNECTING, WRTC_CONNECTION_REQUESTED, WRTC_DISCONNECT, WRTC_DISCONNECTED, WRTC_ICE_CANDIDATE, WRTC_OFFER, WRTC_RENEGOTIATION, WRTC_UPDATE_CONNECTION_STATE, WS_SEND_MESSAGE } from "../../helpers/enums"
 import {
     RTCPeerConnection,
     RTCIceCandidate,
@@ -12,7 +12,7 @@ import {
 } from 'react-native-webrtc'
 
 
-let peerConnecton = null
+let peerConnection
 
 const configuration = {
     iceServers: [
@@ -23,104 +23,122 @@ const configuration = {
 export const webrtcMiddleware = store => next => action => {
     const { dispatch } = store
     switch (action.type) {
+        case WRTC_ADD_TRACK:
+            mediaDevices.getUserMedia({ audio: true, video: false }).then(stream => {
+                // console.log(stream)
+                peerConnection.addStream(stream)
+            }).catch(err => console.log("ERR WITH STREAM", err))
+
+            break
         case WRTC_DISCONNECT:
-            peerConnecton.close()
+            if (peerConnection) peerConnection.close()
             dispatch({
                 type: WRTC_UPDATE_CONNECTION_STATE,
                 payload: WRTC_DISCONNECTED
             })
             break
         case WRTC_CONNECT:
-            peerConnecton = new RTCPeerConnection(configuration)
+            peerConnection = new RTCPeerConnection(configuration)
 
-            peerConnecton.createDataChannel("yeet")
+            mediaDevices.getUserMedia({ audio: true, video: false }).then(stream => {
+                peerConnection.addStream(stream)
 
-            mediaDevices.getUserMedia({ audio: true, video: false }).then(stream => peerConnecton.addStream(stream)).catch(err => console.log("ERR WITH STREAM"))
 
-            peerConnecton.onicecandidate = (e) => {
-                if (e.candidate) {
+                // peerConnection.createDataChannel("yeet")
+
+                peerConnection.onicecandidate = (e) => {
+                    if (e.candidate) {
+                        dispatch({
+                            type: WS_SEND_MESSAGE,
+                            payload: {
+                                event: WRTC_ICE_CANDIDATE,
+                                data: JSON.stringify(e.candidate)
+                            }
+                        })
+                    }
+                }
+
+                // Messes up the local sdp for now.
+                // peerConnection.onnegotiationneeded = () => {
+                //     peerConnection.createOffer().then(offer => {
+                //         ldesc = new RTCSessionDescription(offer)
+                //         peerConnection.setLocalDescription(ldesc)
+                //         dispatch({
+                //             type: WS_SEND_MESSAGE,
+                //             payload: {
+                //                 Event: WRTC_RENEGOTIATION,
+                //                 Data: JSON.stringify(ldesc)
+                //             }
+                //         })
+                //     }).catch(err => console.log("NEG NEED C OFF: ", err))
+                // }
+
+                peerConnection.onconnectionstatechange = (e) => {
+                    let conState
+                    switch (e.currentTarget.connectionState) {
+                        case "connecting":
+                            conState = WRTC_CONNECTING
+                            break;
+
+                        case "connected":
+                            conState = WRTC_CONNECTED
+                            break;
+                        default:
+                            conState = WRTC_DISCONNECTED
+                            break;
+                    }
+
+                    console.log("connection state change ", e.currentTarget.connectionState)
+                    dispatch({
+                        type: WRTC_UPDATE_CONNECTION_STATE,
+                        payload: conState
+                    })
+                }
+
+                peerConnection.createOffer().then(offer => {
+                    ldesc = new RTCSessionDescription(offer)
+                    peerConnection.setLocalDescription(ldesc)
+                    console.log("OFFER PAYLOAD ", ldesc)
+
                     dispatch({
                         type: WS_SEND_MESSAGE,
                         payload: {
-                            event: WRTC_ICE_CANDIDATE,
-                            data: JSON.stringify(e.candidate)
+                            event: WRTC_OFFER,
+                            data: JSON.stringify(ldesc)
                         }
                     })
-                }
-            }
+                })
 
-            // Messes up the local sdp for now.
-            // peerConnecton.onnegotiationneeded = () => {
-            //     peerConnecton.createOffer().then(offer => {
-            //         peerConnecton.setLocalDescription(offer)
-            //         dispatch({
-            //             type: WS_SEND_MESSAGE,
-            //             data: {
-            //                 Event: WRTC_RENEGOTIATION,
-            //                 Data: JSON.stringify(offer)
-            //             }
-            //         })
-            //     })
-            // }
-
-            peerConnecton.onconnectionstatechange = (e) => {
-                let conState
-                switch (e.currentTarget.connectionState) {
-                    case "connecting":
-                        conState = WRTC_CONNECTING
-                        break;
-
-                    case "connected":
-                        conState = WRTC_CONNECTED
-                        break;
-                    default:
-                        conState = WRTC_DISCONNECTED
-                        break;
-                }
-
-                console.log("connection state change ", e.currentTarget.connectionState)
                 dispatch({
                     type: WRTC_UPDATE_CONNECTION_STATE,
-                    payload: conState
+                    payload: WRTC_CONNECTING
                 })
-            }
 
-            peerConnecton.createOffer().then(offer => {
-                ldesc = new RTCSessionDescription(offer)
-                peerConnecton.setLocalDescription(ldesc)
-                console.log("OFFER PAYLOAD ", ldesc)
-
-                dispatch({
-                    type: WS_SEND_MESSAGE,
-                    payload: {
-                        event: WRTC_OFFER,
-                        data: JSON.stringify(offer)
-                    }
-                })
             })
 
-            dispatch({
-                type: WRTC_UPDATE_CONNECTION_STATE,
-                payload: WRTC_CONNECTING
-            })
 
             break
 
         case WRTC_ANSWER:
             rdesc = new RTCSessionDescription(JSON.parse(action.payload))
             console.log("ANSWER PAYLOAD ", JSON.parse(action.payload))
-            peerConnecton.setRemoteDescription(rdesc).then(resp => console.log(resp))
+            peerConnection.setRemoteDescription(rdesc).catch(err => {
+                console.log(err.message),
+                    dispatch({
+                        type: WRTC_DISCONNECT
+                    })
+            })
             break
         case WRTC_ICE_CANDIDATE:
             candidate = new RTCIceCandidate(JSON.parse(action.payload))
             // console.log(action.payload)
-            peerConnecton.addIceCandidate(candidate).then(resp => {
+            peerConnection.addIceCandidate(candidate).then(resp => {
                 // console.log(resp)
             }).catch(fail => console.log(fail))
             break
-        // case WRTC_RENEGOTIATION:
-        //     console.log("RENEGO: ", action.payload)
-        //     break
+        case WRTC_RENEGOTIATION:
+            console.log("RENEGO: ", action.payload)
+            break
         default:
             next(action)
     }

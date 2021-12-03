b0804c4c0e3779ed2a8658efca046d2ed22b2a5b
evanboardway <#Letmein123>
2021-12-03 15:16:28 -0500

    save state

--- src/components/map.js
+++ src/components/map.js
@@ -2,13 +2,34 @@ import React from 'react';
 import { StyleSheet, View, Text, Image } from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import { connect } from 'react-redux';
+import { CONTROLS_BUTTON, CONTROLS_THEME, RED_ACCENT } from '../assets/themes';
 
 const IMAGE_PIGGY = require("../assets/images/piggy.png")
 
 MapboxGL.setAccessToken('pk.eyJ1IjoidGVzc29yby0iLCJhIjoiY2t1b3EzY2d2MGV1ejJ2bzFtbXIxMmdjbCJ9.P-4uyej3lnQVxNs1Tzc-Sw');
 
-const Map = (props) => {
+const renderPeerMarkers = (peerLocations) => {
+  let markers = []
+  peerLocations.forEach((location, uuid) => {
 
+    markers.push(
+      <MapboxGL.MarkerView
+          id={uuid}
+          coordinate={location.longitude, location.latitude}>
+          <View style={styles.iconWrapper}>
+            <Image
+              source={IMAGE_PIGGY}
+              style={styles.icon}
+            />
+          </View>
+        </MapboxGL.MarkerView>
+    )
+
+  })
+  return markers
+}
+
+const Map = (props) => {
   return (
     <View style={styles.map}>
       <MapboxGL.MapView
@@ -26,16 +47,9 @@ const Map = (props) => {
           followZoomLevel={16}
         >
         </MapboxGL.Camera>
-        {/* <MapboxGL.UserLocation /> */}
+        <MapboxGL.UserLocation />
 
-        <MapboxGL.MarkerView
-          id={"myid"}
-          coordinate={[-122.0312186, 37.33233141]}>
-          <Image
-            source={IMAGE_PIGGY}
-            style={styles.icon}
-          />
-        </MapboxGL.MarkerView>
+        {renderPeerMarkers(props.peerLocations)}
 
 
       </MapboxGL.MapView>
@@ -50,22 +64,23 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   icon: {
-    height: 20,
-    width: 20,
+    height: 15,
+    width: 15,
     opacity: 1,
     resizeMode: 'contain',
-    backgroundColor: 'rgba(3, 240, 252, 0.8)',
-    borderRadius: 10,
-    borderWidth: 20,
-    borderColor: 'rgba(3, 240, 252, 0.7)'
-
+    borderRadius: 10
   },
+  iconWrapper: {
+    backgroundColor: 'rgb(255, 179, 0)',
+    padding: 5,
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: 'rgba(255,255,255,0.7)'
+  }
 });
 
 const mapStateToProps = (state) => {
-  console.log(state.currentLocation)
-  return { location: [state.currentLocation.latitude, state.currentLocation.longitude] }
-  // return { location: [37.33233141, -122.0312186] }
+  return { peerLocations: state.peerLocations  }
 };
 const connectComponent = connect(mapStateToProps);
 export default connectComponent(Map);
